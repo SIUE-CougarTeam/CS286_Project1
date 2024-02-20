aa9c480d8474367d6523dfe3dce08d84f8682c07
Sage Phillips <25728744+sphills@users.noreply.github.com>
2024-02-20 10:17:44 -0600

    Adding binary to text file

---
+++ b2t.c
@@ -0,0 +1,47 @@
+
+/*******************************************************
+This program converts a binary file to a file 
+containing string representations of the 1's and 0's
+in the binfary file. 
+*******************************************************/
+
+// gcc -o b2t b2t.c
+// Usage ./a.out < inputfile > outputfile
+
+#include<stdio.h>
+
+main( int argc, char** argv) 
+{
+
+    if ( argc >1 )
+	fprintf(stderr,"Usage: ./a.out < input_txt > output_bin\n");
+    char word[32];
+    unsigned char vals[4];
+    unsigned char w, div, b;
+    unsigned char tot ;
+    
+    w = 0;
+    while( scanf( "%c", &tot) != EOF )
+    {
+	    div = 128;
+	    for( b=0; b<8; b++ )
+	    {
+		if( tot >= div)
+		{
+		    printf( "1" );
+		    tot -= div;
+		}
+		else
+		    printf( "0");
+		div = div/2;
+	    }
+	   w ++;
+	   if ( w == 4 )
+	   {
+	       printf("\n");
+	       w = 0;
+	   }
+
+    }
+
+}
