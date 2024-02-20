c9a1b9c3faa0b74adcdbceb78f861dbd7a51d2bf
Sage Phillips <sagphil@vm-02.cs.siue.edu>
2024-02-20 12:32:22 -0600

    Printing all instructions to screen.

--- startSim.cpp
+++ startSim.cpp
@@ -32,25 +32,28 @@ int main()
                         iPtr[1] = buffer[2];
                         iPtr[2] = buffer[1];
                         iPtr[3] = buffer[0];
-                	item I;
+                	item instruction;
 			unsigned int asUint = (unsigned int) i;
-			I.asUint = asUint;
+			instruction.asUint = asUint;
 			bitset<32> b( i );
-			I.binStr = b.to_string();
-			I.valid = asUint >> 31;
-			I.opcode = asUint >> 26;
-			I.rs = (asUint << 6)>>27;
-			I.rt = (asUint << 11)>>27;
-			I.imm = (i << 16) >> 16;
+			instruction.binStr = b.to_string();
+			instruction.valid = asUint >> 31;
+			instruction.opcode = asUint >> 26;
+			instruction.rs = (asUint << 6)>>27;
+			instruction.rt = (asUint << 11)>>27;
+			instruction.imm = (i << 16) >> 16;
 			//cout << "valid bit: " << valid << endl;
 			//cout << "opcode: " << opcode << endl;
 			//cout << binstr << "\t"; 
-			if( I.opcode == 40 ){
-				I.instrStr ="ADDI\tR" + to_string(I.rt) + ", R"
-				       + to_string(I.rs) + ", #" +to_string(I.imm);
-				cout << I.binStr << "\t" << I.instrStr << endl;
+			if( instruction.opcode == 40 ){
+				instruction.instrStr = "ADDI\tR" + to_string(instruction.rt) + ", R"
+				       + to_string(instruction.rs) + ", #" +to_string(instruction.imm);
+				cout << instruction.binStr << "\t" << instruction.instrStr << endl;
+			} else {
+				instruction.instrStr = to_string(instruction.rt) + " " + to_string(instruction.rs) + " " + to_string(instruction.imm);
+				cout << instruction.binStr << "\t" << instruction.instrStr << endl;
 			}
-			MEM[addr] = I;
+			MEM[addr] = instruction;
 			addr+=4;
 		}
         } // end of decode
@@ -62,16 +65,16 @@ int main()
 
 	while( true ){
 		
-		item I = MEM[PC];
-		while( I.valid == 0 ){
+		item instruction = MEM[PC];
+		while( instruction.valid == 0 ){
 			PC +=4;
-			I = MEM[PC];
+			instruction = MEM[PC];
 		}
-		if(I.opcode == 40 ) {
-			R[I.rt] = R[I.rs] + I.imm;
+		if(instruction.opcode == 40 ) {
+			R[instruction.rt] = R[instruction.rs] + instruction.imm;
 		}	
 		cout << "====================\ncycle:" + to_string(cycle) 
-			+ " " + to_string(PC) +"\t" + I.instrStr + " " + "\n\nregisters:\n"
+			+ " " + to_string(PC) +"\t" + instruction.instrStr + " " + "\n\nregisters:\n"
 			+ to_string(R[0]) + " " + to_string(R[1]) +"\n";
 			
 
@@ -80,6 +83,4 @@ int main()
 
 		if( cycle >= 2) break;
 	}
-
-
 }
